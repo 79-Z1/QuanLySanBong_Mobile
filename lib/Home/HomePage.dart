ea691c701ff6d3effcdc0cab406bd641a73eb315
TeienFat <tiendb505@gmail.com>
2023-05-12 14:49:19 +0700

    update HomePage

--- lib/Home/HomePage.dart
+++ lib/Home/HomePage.dart
@@ -9,29 +9,78 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   int indexBar = 0;
+  String userName = "Trương Khánh Hòa";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 80,
+              decoration: BoxDecoration(
+                  color: Colors.green,
 
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Trang chủ"),
-          BottomNavigationBarItem(icon: Icon(Icons.account_circle_outlined), label: "Tài khoản"),
-          BottomNavigationBarItem(icon: Icon(Icons.history_rounded), label: "Lịch sử"),
-          // BottomNavigationBarItem(icon: Icon(Icons.newspaper), label: "Tin tức"),
-        ],
-        currentIndex: indexBar,
-        onTap: (value) {
-          indexBar = value;
-          setState(() {
-          });
-          switch(value){
-            case 0: _showSnackbar(context, "Bạn mở Inbox"); break;
-            case 1: _showSnackbar(context, "Bạn mở Contacts"); break;
-            case 2: _showSnackbar(context, "Bạn mở Calender"); break;
-            case 3: _showSnackbar(context, "Bạn mở Calender"); break;
-          }
-        },
+              ),
+              child: Row(
+                children: [
+                  Icon(Icons.account_circle,size: 50,color: Colors.white,),
+                  SizedBox(width: 5,),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("Xin chào",
+                        style: TextStyle(
+                            fontSize: 10,
+                            color: Colors.white
+                        ),
+                      ),
+                      Text("${userName}",
+                          style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white)
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ],
+        )
+        ,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home,color: Colors.green,),
+                label: "Trang chủ",
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.account_circle,color: Colors.green),
+                label: "Tài khoản",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.history_outlined,color: Colors.green),
+              label: "Lịch sử",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.newspaper,color: Colors.green),
+              label: "Tin tức",
+            ),
+          ],
+          type: BottomNavigationBarType.shifting,
+          selectedLabelStyle: TextStyle(fontWeight: FontWeight.bold),
+          selectedItemColor: Colors.green,
+          currentIndex: indexBar,
+          iconSize: 40,
+          onTap: (value) {
+            indexBar = value;
+            setState(() {
+
+            });
+          },
       ),
     );
   }
