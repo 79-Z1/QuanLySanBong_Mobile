23354fb597959a237c5f9a01cd54b897a89bd4e8
TeienFat <tiendb505@gmail.com>
2023-05-28 09:38:24 +0700

    xong khuya ngày 28/05 + thêm file giá + sửa 1 chút booking + xử lý đặt

--- lib/Booking/CostTablePage.dart
+++ lib/Booking/CostTablePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:quanlysanbong/Firebase/Connect_Firebase.dart';
 import 'package:quanlysanbong/Firebase/JoinTable.dart';
 
@@ -30,44 +31,91 @@ class PageCostTable extends StatefulWidget {
 
 class _PageCostTableState extends State<PageCostTable> {
   String? maSan;
+  final NumberFormat usCurrency = NumberFormat('#,##0', 'en_US');
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Bảng giá sân"),
-      ),
-      body:StreamBuilder(
-        stream: JoinTable.BangGiaSanFromMaSan(maSan!),
-        builder: (context, snapshot) {
-          if(snapshot.hasError) {
-            print("Lỗi nè");
-            print(snapshot.error);
-            return const Center(
-              child: Text("Lỗi dữ liệu Firebase",
-                style: TextStyle(color: Colors.red),
+        title: Row(
+          children: [
+            SizedBox(width: 60,),
+            Text("Bảng giá sân",
+              style: TextStyle(
+                fontSize: 25,
               ),
-            );
-          } else if(!snapshot.hasData) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }else {
-            var list = snapshot.data!;
-            return ListView.separated(
-              itemBuilder: (context, index) =>
-                  Container(
-                    child: Row(
-                      children: [
-                        Text("${list[index]['Gio']}"),
-                        Text("${list[index]['GiaTheoGio']}"),
-                      ],
+            ),
+          ],
+        ),
+      ),
+      body:Column(
+        children: [
+          SizedBox(height: 20,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Text("Giờ",
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text("Giá theo giờ",
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 20,),
+          Expanded(
+            child: StreamBuilder(
+              stream: JoinTable.BangGiaSanFromMaSan(maSan!),
+              builder: (context, snapshot) {
+                if(snapshot.hasError) {
+                  print("Lỗi nè");
+                  print(snapshot.error);
+                  return const Center(
+                    child: Text("Lỗi dữ liệu Firebase",
+                      style: TextStyle(color: Colors.red),
                     ),
-                  ),
-              separatorBuilder: (context, index) => Divider(thickness: 2),
-              itemCount: list.length,
-            );
-          }
-        },
+                  );
+                } else if(!snapshot.hasData) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }else {
+                  var list = snapshot.data!;
+                  return ListView.separated(
+                    itemBuilder: (context, index) =>
+                        Container(
+                          height: 50,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Text("${list[index]['Gio']}",
+                                style: TextStyle(
+                                  fontSize: 25,
+
+                                ),
+                              ),
+                              Text("${usCurrency.format(int.parse(list[index]['GiaTheoGio']))} vnđ",
+                                style: TextStyle(
+                                  fontSize: 25,
+
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                    separatorBuilder: (context, index) => Divider(thickness: 2),
+                    itemCount: list.length,
+                  );
+                }
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
